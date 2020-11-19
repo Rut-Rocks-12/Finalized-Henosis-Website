48fa21c9e002f3a1dde792238969bb356773b05c
Rut-Rocks-12 <54673663+Rut-Rocks-12@users.noreply.github.com>
2020-11-19 22:35:19 +0100

    Javascripting

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-tl = new TimelineMax ({});
-tlafter = new TimelineMax ({});
-tl2 = new TimelineMax ({});
+sectionHomeTitleImage = new TimelineMax ({});
+hidetext2DelayedSlideUp = new TimelineMax ({});
+hidetext3DelayedSlideRight = new TimelineMax ({});
 
-tl.from('.hidetext', 2.5, {y:"100%", ease:Power4.easeOut, delay: 2})
-tlafter.from('.title', 1.25, {opacity:0, ease:Power4.easeOut})
-.from('.homeimg', 1.25, {opacity:0, ease:Power4.easeOut})
-tl2.from('.hidetext2',2.5, {y:"100%", ease:Power4.easeOut, delay: 2})
-const scrollText = document.querySelector(".hidetext1");
+
+sectionHomeTitleImage.from('.home-section-title-container', 1.25, {opacity:0, ease:Power4.easeOut})
+.from('.home-section-landing-image', 1.25, {opacity:0, ease:Power4.easeOut})
+hidetext2DelayedSlideUp.from('.hidetext2',2.5, {y:"100%", ease:Power4.easeOut, delay: 2})
+hidetext3DelayedSlideRight.from('.hidetext3', 2.5, {x:"100%", ease:Power4.easeOut, delay: 3})
+
+const scrollText = document.querySelector(".home-section-title");
+var SectionInitativesHeight = document.querySelector(".Class-iniatives").getBoundingClientRect().top
+var SectionContactUsHeight = document.querySelector(".Class-contact").getBoundingClientRect().top
+
 
 window.addEventListener("scroll", () => {
-  scrollText.style.left = `${0 + 0.3 * scrollY}px`;
+  scrollText.style.left = `${0.3 * scrollY}px`;
+  if (scrollY > SectionInitativesHeight) {
+    hidetext3DelayedSlideRight.to('.hidetext3', 1.5, {x:"100%", ease:Power4.easeOut});
+  } 
+  if (scrollY > SectionContactUsHeight) {
+    hidetext3DelayedSlideRight.from('.hidetext3', 1.5, {x:"100%", ease:Power4.easeOut});
+  }
+  
 });   
 
 const Navmenu = document.getElementById("section-navigation-menu-container-id");
